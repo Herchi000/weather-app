7d37100c1029b1ba3ec18107da9b677962b938e6
Emanuel <hugofer000@gmail.com>
2022-01-20 16:24:42 -0300

    Basic React Project with HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="container">
+        
+        <main className="main-container">
+            
+            <div className="div-search">
+                <input type="text" placeholder="Nombre de Ciudad..." className="input-city" id="input-city"/>
+                <button className="btn-search" id="btn-search"><i className="fas fa-search"></i></button>
+            </div>
+           
+            <span className="loading hidden">Cargando...</span>
+            <span className="error-message hidden"></span>
+
+            <div className="weather hidden">
+                <div className="weather-region">
+                    <span className="name-city">Reconquista</span>
+                    <span className="name-country">Arg</span>
+                </div>
+                
+    
+                <div className="weather-temp">
+                    <span className="current-temp">31°C</span>
+                    <span className="feelslike-temp">ST 37 °C</span>
+                    <span className="min-temp">25 °C</span>
+                    <span className="max-temp">37 °C</span>
+                </div>
+    
+                <div className="weather-condition">
+                    <span className="weather-text">Clear</span>
+                    <img src="" alt="weather" className="weather-img"/>
+                    <span className="wind-speed">12 Kh</span>
+                    <span className="wind-direction">N</span>
+                    <span className="humidity">45%</span>
+                </div>
+    
+    
+    
+            </div>
+        </main>
+        
     </div>
   );
 }
